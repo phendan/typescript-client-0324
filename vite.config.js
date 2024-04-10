d1940ed014e399f9d4e2767d89bd4c58f1f239e4
phendan <10546560+phendan@users.noreply.github.com>
2024-04-10 22:03:27 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+/** @type {import('vite').UserConfig} */
+export default {
+    server: {
+        host: 'typescript-client.local',
+        port: 80
+    },
+    build: {
+        outDir: './build'
+    }
+};
